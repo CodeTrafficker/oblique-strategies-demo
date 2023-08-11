58cd5dff85d823c2af481f524db20d897e66b516
Phil at Xonox <36802525+CodeTrafficker@users.noreply.github.com>
2023-08-11 16:05:40 -0700

    Update page.tsx
    With no initial pullObliqueStrategy fn run, this displays with no console errors. Message is only in console, not in p tag above button.

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function obliqueStrategies()  {
   }
 
   // run on load, then on clicks:
-  pullObliqueStrategy();
+  // pullObliqueStrategy();
   
   return (
     <div className="">
