23134f2032f9172e9cbfe48c8b7e821f84037a09
Phil at Xonox <36802525+CodeTrafficker@users.noreply.github.com>
2023-08-11 21:08:46 -0700

    Update page.tsx
    Message empty on load;
    Clicking button adds random msg to div, above.
    Code cleanup.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-// import allStrategies from '../data/data.json';
+import {useState} from 'react';
 import './page.scss';
 
 /////// Notes: //////
@@ -8,49 +8,39 @@ import './page.scss';
 // Put json in /data/
 // 115 strategies total
 
-
-// These two need the allStrategies import, above:
-// This dumps out full json, formatted:
+//// These two need the allStrategies import: /////
+// import allStrategies from '../data/data.json';
+// 1) This dumps out full json, formatted:
 // console.log('allStrategies: ', allStrategies);
-
-// Ex: Pick the third one: 
 // console.log('oblique3: ', allStrategies[4]);
-// but for some reason could not extract .length from allStrategies obj (2 instead of 115)
-
+// but for some reason could not extract allStrategies.length obj (2 instead of 115)
 
 
 export default function obliqueStrategies()  {
+  const [theMessage, setMessage] = useState();
 
-  // Now .length works: 
+  // Now xx.length works: 
   const strategiesJSON = require('../data/data.json');
   // console.log('strategiesJSON.length: ', strategiesJSON.length );
-
-  let theMessage: string;  
 
   const randomizer = (max) => {
     return Math.floor(Math.random() * (max) )
   }
 
   const pullObliqueStrategy = () => {
-
     let randomNum = randomizer( strategiesJSON.length );
     let thisRandomStrategy = strategiesJSON[randomNum].strategy;
     console.log('randomNum: ', randomNum );
     console.log('thisRandomStrategy: ', thisRandomStrategy );
 
-    // theMessage = thisRandomStrategy;
-    return theMessage = thisRandomStrategy;
+    setMessage(prevMsg => thisRandomStrategy);
   }
 
-  // run on load, then on clicks:
-  // pullObliqueStrategy();
-  
+
   return (
     <div className="">
-      <p id="theMessage">{theMessage} </p>
-       <button className="primary" id="" onClick={pullObliqueStrategy}>New Strategy, please</button>
+      <p id="theMessage">The Message: {theMessage} </p>
+      <button className="primary" id="" onClick={pullObliqueStrategy}>New Strategy, please</button>
     </div>
-
   );
-
 }
