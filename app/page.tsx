9a4c7505f66d6abcd2689791e389f94861df0ab8
Phil at Xonox <36802525+CodeTrafficker@users.noreply.github.com>
2023-08-24 23:59:01 -0700

    Styling media query for text and container sizes, useEffect for page load
    - useEffect to load message on page load instead of blank;
    - Scheme to set 640 and above styles with media query, outside of Tailwind's preset utilitys classes.
    - more styling to look better.
    - general code clean-up, fixed smart single quote in data.json, etc

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,32 @@
 'use client';
 
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './page.scss';
 
 /////// Notes: //////
 // Don't use getInitialProps bc of React's /app vs /pages brittleness
 // Put json in /data/
-// 115 strategies total
+// 114 strategies total, but get length() for versality's sake
 
-//// These two need the allStrategies import: /////
-// import allStrategies from '../data/data.json';
-// 1) This dumps out full json, formatted:
-// console.log('allStrategies: ', allStrategies);
-// console.log('oblique3: ', allStrategies[4]);
-// but for some reason could not extract allStrategies.length obj (2 instead of 115)
-
+// Can go into global functions file if expanding later:
+const randomizer = (max) => {
+  return Math.floor(Math.random() * (max) )
+}
 
 export default function obliqueStrategies()  {
   const [theMessage, setMessage] = useState();
-
-  // Now xx.length works: 
   const strategiesJSON = require('../data/data.json');
-  // console.log('strategiesJSON.length: ', strategiesJSON.length );
+  const jsonLength = strategiesJSON.length;
+  console.log('const jsonLength is: ', jsonLength );
 
-  const randomizer = (max) => {
-    return Math.floor(Math.random() * (max) )
-  }
+  // Run once on page load to show an initial strategy message instead 
+  // of empty space. Empty array [] because React:
+  useEffect(() => {
+    pullObliqueStrategy();
+  }, []);
 
   const pullObliqueStrategy = () => {
-    let randomNum = randomizer( strategiesJSON.length );
+    let randomNum = randomizer( jsonLength );
     let thisRandomStrategy = strategiesJSON[randomNum].strategy;
     console.log('randomNum: ', randomNum );
     console.log('thisRandomStrategy: ', thisRandomStrategy );
@@ -39,8 +37,8 @@ export default function obliqueStrategies()  {
   return (
     <>
     <h1 className="text-center">OBLIQUE STRATEGIES</h1>
-    <div className="app-container mx-auto px-5 text-center">
-      <p id="theMessage">{theMessage}</p>
+    <div className="app-container   mx-auto px-5 text-center">
+      <p className="theMessage">{theMessage}</p>
       <button className="primary absolute bottom-10" onClick={pullObliqueStrategy}>New Strategy, please</button>
     </div>
     </>
